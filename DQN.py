4d123a1a35cc18b5518fe71383c6f5d1dca6f579
guoqikai <qikai.guo@mail.utoronto.ca>
2021-07-20 14:42:53 -0400

    clean code

--- DQN.py
+++ DQN.py
@@ -23,22 +23,20 @@ class AutoDriveDQN:
         self.target_net.eval()
         state, action, reward, next_state = memory.sample_from_experience(sample_size=batch_size)
         expected = self.policy_net(state)[torch.arange(action.shape[0]), action.long()]
-        target, _ = torch.max(self.target_net(next_state).detach(), axis=1)
+        target, _ = torch.max(self.target_net(next_state).detach(), dim=1)
         self.policy_net.train()
         loss = self.loss_fn(expected.unsqueeze(1), (reward + self.gamma * target).unsqueeze(1))
         return loss
 
 
 class QNet(nn.Module):
-    def __init__(self, conv_layer_sizes, linear_layer_sizes, batch_norm=True):
+    def __init__(self, conv_layer_sizes, linear_layer_sizes):
         super().__init__()
 
         cnn_layers = []
         for i in range(len(conv_layer_sizes)):
             in_c, out_c, kernel, stride = conv_layer_sizes[i] 
             cnn_layers.append(nn.Conv2d(in_c, out_c, kernel, stride=stride))
-            if batch_norm:
-                cnn_layers.append(nn.BatchNorm2d(conv_layer_channels[i]))
             cnn_layers.append(nn.ReLU())
 
         layers = []
@@ -54,9 +52,9 @@ class QNet(nn.Module):
     inputs is batch_size * img_per_state * C * H * W
     """
     def forward(self, states):
-        if len(states.shape) == 4:
-            inputs = inputs.unsqueeze(0)
-        B, _, C, H, W = states.shape
-        return self.dnn(self.cnn(states.view(-1, C, H, W)).view(B, -1))
+        if len(states.shape) == 5:
+            states = torch.sum(states, dim=2)
+        B, C, H, W = states.shape
+        return self.dnn(self.cnn(states).view(B, -1))
 
 
--- train.py
+++ train.py
@@ -13,21 +13,20 @@ if __name__ == "__main__":
     # initialize the game
     car = Car(0, (100, 100), (33, 43), (255, 125, 0), 4, 7, 14, 0.01)
     map_ = np.zeros((640, 640, 3))
-   
 
     game = AutoDriveGame(map_, car, (500, 500), max_steps=512)
 
     # initialize the DQA and agent
-    q_net = QNet([(3, 8, 8, 4), (8, 16, 4, 4), (16, 16, 3, 2)], [576, 256, len(action_space)], batch_norm=False).to(device)
+    q_net = QNet([(4, 16, 8, 4), (16, 32, 4, 4), (32, 32, 3, 2)], [800, 256, len(action_space)]).to(device)
     dqn = AutoDriveDQN(q_net, 0.75, 100, torch.nn.MSELoss())
     agent = DQNAgent(dqn)
 
-    epsilon = 1
+    epsilon = 0.5
     exp_replay_size = 4096
     sampling_interval = 1
     sample_size = 128
     state_interval = 4
-    img_sz = (120, 120)
+    img_sz = (180, 180)
     memory = Experience(game, exp_replay_size, sampling_interval, state_interval, img_sz, device)
     episodes = 100
 
@@ -57,12 +56,13 @@ if __name__ == "__main__":
             continue
 
         for i in range(sample_size):
-            for im in memory.experience_replay[len(memory.experience_replay)  - sample_size + i][0][0].cpu().numpy().transpose(0, 2, 3, 1):
+            for im in memory.experience_replay[len(memory.experience_replay) - sample_size + i][0][0].cpu().numpy().transpose(0, 2, 3, 1):
                 surf = pygame.surfarray.make_surface(im * 255)
                 display.blit(surf, (0, 0))
                 pygame.display.update()
                 pygame.time.wait(10)
-torch.save(dqn.policy_net.state_dict(), "./policy")
-if show_game:
-    pygame.quit()
+    torch.save(dqn.policy_net.state_dict(), "./policy")
+    if show_game:
+        pygame.quit()
+
 
